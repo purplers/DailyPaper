51dc2cfb04982125d003ae995c884ce5619fa3d8
purple <904839562@qq.com>
2023-05-02 16:19:13 +0800

    完成顶部导航栏布局

--- app/src/main/java/com/example/dailypaper/MainActivity.kt
+++ app/src/main/java/com/example/dailypaper/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.dailypaper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toolbar
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.dailypaper.databinding.ActivityMainBinding
 import androidx.databinding.DataBindingUtil
 import com.example.dailypaper.viewmodel.NewsViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +27,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // 获取当前日期并将其格式化为所需的字符串格式
+        val sdf = SimpleDateFormat("MM月dd日", Locale.getDefault())
+        val currentDate = sdf.format(Date())
+
+// 找到 Toolbar 中的 Button 并设置当前日期
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val dateBtn = toolbar.findViewById<Button>(R.id.date_btn)
+        dateBtn.text = currentDate
     }
 }
--- app/src/main/java/com/example/dailypaper/MyApplication.kt
+++ app/src/main/java/com/example/dailypaper/MyApplication.kt
@@ -9,8 +9,9 @@ import com.example.dailypaper.db.NewsDatabase
  *email：904839562@qq.com
 date : 2023/4/29 17:59
  */
-class MyApplication {
 
+//在Application中实例化，方便我们统一管理和全局调用，且不用担心生命周期问题，通常伴随着整个应用程序的生命周期
+class MyApplication {
     companion object {
         val newsDatabase: NewsDatabase? = null
         val api: Api? = null
